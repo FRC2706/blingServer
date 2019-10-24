20ad06a20f5a7b890521f42fad3eccfc6f163db0
Ken James <kjames@ciena.com>
2019-10-24 08:54:51 -0400

    Enhance the bling command input in nt_robot.py
    Seven parameters need to be sent to the blingServer and entering them as
    a comma-separated string is error prone (even if you can remember the
    correct order). This update changes the user interface to prompt for
    each individual parameter, validate it, and save it so that it need not
    be typed the next time a command is sent (unless it needs to be changed)

--- nt_robot.py
+++ nt_robot.py
@@ -18,41 +18,155 @@ import sys
 import time
 from networktables import NetworkTables
 
+import os
+import json
+import collections
+
 # To see messages from networktables, you must setup logging
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
+
+def terminate(return_code):
+    """Terminate the network tables server gracefully"""
+
+    NetworkTables.shutdown()
+    sys.exit(return_code)
+
+def get_integer(var_name, default_value, min_value, max_value):
+    """Get an integer value between a specified minimum and maximum value
+       from the user and return it (sending the default if the user inputs
+       nothing)
+    """
+
+    while True:
+        try:
+            tmp = raw_input("Enter %s value [%d]: " % (var_name, default_value))
+            if tmp == '':
+                return default_value
+            if not tmp.isdigit():
+                print("*** value must be an integer in the range [%s-%s] ***" %(min_value, max_value))
+            else:
+               value = int(tmp)
+               if value >= min_value and value <= max_value:
+                   return value
+                   break
+               else:
+                   print("*** value must be in the range [%s-%s] ***" %(min_value, max_value))
+        except KeyboardInterrupt as e:
+            print('keyboard interrupt in get_integer')
+            terminate(0)
+
+def get_string(var_name, valid_values, saved_value):
+    """Get a string value from the user and verify it is one
+       of the valid_values and return it (sending the saved_value if
+       the user inputs nothing
+    """
+
+    while True:
+        try:
+            cmd = raw_input("Enter %s [%s]: " % (var_name, saved_value))
+            if cmd == '':
+                return saved_value
+
+            for value in valid_values:
+                if cmd == value:
+                    return cmd
+            print("*** command must be one of %s" % valid_values)
+
+        except KeyboardInterrupt as e:
+            print('keyboard interrupt in get_string')
+            terminate(0)
+
 
 NetworkTables.initialize()
 sd = NetworkTables.getTable("blingTable")
 
+# The place we will squirrel away the last set of values passed to the
+# bling server
+saved_values_file = '/tmp/nt_robot_saved'
+
+# We use an ordered dict here so that the user is always prompted for
+# values in the same order (regular dicts don't retain key order)
+# * NOTE * command should always be last as this is the value that
+# the blingServer is waiting to see change before it reads any of the
+# others
+defaults = collections.OrderedDict([
+('red',         {"default": 255, "min": 0, "max": 255}),
+('green',       {"default": 255, "min": 0, "max": 255}),
+('blue',        {"default": 255, "min": 0, "max": 255}),
+('repeat',      {"default": 100, "min": 1, "max": 1000}),
+('wait (ms)',   {"default": 10,  "min": 0, "max": 100}),
+('brightness',  {"default": 128, "min": 0, "max": 255}),
+('command', 'clear')
+])
+
+# The command strings recognized by the bling server
+commands = [
+"colorWipe",
+"solid",
+"blink",
+"theaterChase",
+"rainbow",
+"rainbowCycle",
+"theaterChaseRainbow",
+"clear"
+]
+
+# Read in the saved defaults if the file exists. Since it is in /tmp it will
+# disappear every time the PI reboots
+
+if os.path.exists(saved_values_file):
+    with open(saved_values_file, 'r') as f:
+        saved_defaults=json.load(f)
+
+        # When the defaults are written to the saved file no key order
+        # is preserved so we have to reassign them one by one into defaults
+        # (which has the correct order)
+        for k in saved_defaults.keys():
+            defaults[k] = saved_defaults[k]
+
 try:
+    # Keep asking for input until the user enters a keyboard interrrupt (<ctrl>C)
+    print("Enter <ctrl>C to exit")
     while True:
-        parms = input("Enter command color: ")
-
+        values_to_print = []
         try:
-            red,green,blue,repeat,wait_ms,LED_BRIGHTNESS,command=parms.split(",")
-        except Exception as e:
-            print("Exception reading data from stdin")
-            print(str(e))
-            NetworkTables.shutdown()
-            sys.exit(0)
+            for key in defaults.keys():
+                if not key == 'command':
+                    value = get_integer(key, defaults[key]["default"],
+                                             defaults[key]["min"],
+                                             defaults[key]["max"])
+                    defaults[key]["default"] = value
+                    values_to_print.append(value)
+                else:
+                    cmd_value = get_string(key, commands, defaults[key])
+                    defaults[key] = cmd_value
+                    values_to_print.append(cmd_value)
 
-        print("Command: %s" % command)
-        print("red:   %s", red)
-        print("green:   %s",  green)
-        print("blue:   %s",  blue)
-        print("Repeat:  %s",  repeat)
-        print("wait_ms:    %s", wait_ms)
-        print("LED_BRIGHTNESS: %s", LED_BRIGHTNESS)
+            print("Sending: Red Green Blue Repeat Wait Brightness Command")
+            print("         %3d %5d %4d %6d %4d %10d %s" % tuple(values_to_print))
 
-        sd.putNumber('red', red)
-        sd.putNumber('green', green)
-        sd.putNumber('blue', blue)
-        sd.putNumber('repeat', repeat)
-        sd.putNumber('wait_ms', wait_ms)
-        sd.putNumber('LED_BRIGHTNESS', LED_BRIGHTNESS)
-        sd.putString('command', command)
+            # We could have sent all of these to the blingServer in the above loop
+            # but don't so that there is not a significant delay (waiting for the
+            # user to enter the next value) between sends
+
+            for key in defaults.keys():
+                if not key == 'command':
+                    #print("sd.putNumber %s, %s" % (key, defaults[key]["default"]))
+                    sd.putNumber(key, defaults[key]["default"])
+                else:
+                    #print("sd.putString %s, %s" % (key, defaults[key]))
+                    sd.putString(key, defaults[key])
+
+            with open(saved_values_file, 'w') as f:
+                json.dump(defaults, f, indent=4)
+
+        except KeyboardInterrupt as e:
+            print("keyboard interrupt in main")
+            terminate(0)
+
 except Exception as e:
+    print("Unexpected exception in main")
     print(str(e))
-    NetworkTables.shutdown()
-    sys.exit(0)
+    terminate(1)
